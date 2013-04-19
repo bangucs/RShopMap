4cff78f3ef2ef92f0035f6cc2b1a2c3fc9fd1e76
mitsuo.hangai <mitsuo.hangai@mail.rakuten.com>
2013-04-19 18:10:19 +0900

    second commit

---
+++ workspace/test/src/com/bangucs/util/Trigram.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.bangucs.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mitsuo.hangai
+ *
+ */
+public class Trigram {
+	/**
+	 * This method purses difference of 2 Strings with Trigram algorithm.
+	 * @param a
+	 * @param b
+	 * @return difference between 2 strings
+	 */
+	public double purseTrigram(String a, String b){
+		double difference = 0;
+		int aLength = a.length();
+		int bLength = b.length();
+		List<String> aArray = new ArrayList<String>();
+		List<String> bArray = new ArrayList<String>();
+		double same = 0;   //同じののかず
+		double denom = 0;  //分母
+		//3文字以内ならその場で比較して返しちゃう
+		if(aLength<=3 || bLength<=3){
+			if(a.equals(b)){
+				same = 1;
+				denom = 1;
+				return 1;
+			}else{
+				return difference;
+			}
+		}
+		for(int i=0;i<aLength-2;i++){
+			aArray.add(i, a.substring(i, i+3));
+		}
+		for(int i=0;i<bLength-2;i++){
+			bArray.add(i, b.substring(i, i+3));
+		}
+		int aSize = aArray.size();
+		int bSize = bArray.size();
+		//compare 2 strings
+
+		if(aSize >= bSize){
+			for(int i =0;i<aSize;i++){
+				for(int j=0;j<bSize;j++){
+					if(aArray.get(i).equals(bArray.get(j))){
+						same ++;
+					}
+				}
+			}
+			denom = aSize;
+		}else{
+			for(int i =0;i<bSize;i++){
+				for(int j=0;j<aSize;j++){
+					if(bArray.get(i).equals(aArray.get(j))){
+						same ++;
+					}
+				}
+			}
+			denom = bSize;
+		}
+		if(same==0){
+			difference = 0;
+		}else{
+			difference = same/denom;
+		}
+		return difference;
+	}
+}
